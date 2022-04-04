264105d3002354c4416eca40361af591a7043990
Jakub Skurčák <jakobskurcak@gmail.com>
2022-04-04 03:06:28 +0200

    Cleanup code

--- app/Mail/ReportMail.php
+++ app/Mail/ReportMail.php
@@ -4,11 +4,9 @@ namespace App\Mail;
 
 use App\Models\Report;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\View\View;
 
 class ReportMail extends Mailable
 {
